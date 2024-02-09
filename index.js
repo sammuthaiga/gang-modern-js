9f776e2971b7b95a6e8e6b20727932f84ccaed35
Samuel <samuel.muthaiga@student.moringaschool.com>
2024-02-10 02:10:31 +0300

    Basic intro to strings in JavaScript

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Learning about strings in javascript
+// Learningc about strings in javascript
+console.log('Hello world');
+let email = 'danretsolutions@gmail.com';
+console.log(email); //testing variables
+
+// string concatination
+let firstName = 'John';
+let lastName = 'Doe';
+let fullName = firstName + ' ' + lastName;
+console.log(fullName);
+// getting characters
+
+console.log(fullName[0]);
+console.log(fullName[1]);
+console.log(fullName[2]);
+console.log(fullName[3]);
+// string length
+console.log(fullName.length);
+console.log(lastName.length);
+console.log(firstName.length);
+// string methods
+console.log(fullName.toUpperCase());
+let result = fullName.toLowerCase();
+console.log(result);
+let index = email.indexOf('@');
+console.log(index);
