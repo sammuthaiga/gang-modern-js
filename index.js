b5e3d74b3cbe23b4fe719f4893dce0c2e4da963d
Samuel <samuel.muthaiga@student.moringaschool.com>
2024-02-10 02:26:52 +0300

    more string methods in javascript

--- index.js
+++ index.js
@@ -1,26 +1,39 @@
-// Learningc about strings in javascript
-console.log('Hello world');
-let email = 'danretsolutions@gmail.com';
-console.log(email); //testing variables
+// // Learningc about strings in javascript
+// console.log('Hello world');
+// let email = 'danretsolutions@gmail.com';
+// console.log(email); //testing variables
 
-// string concatination
-let firstName = 'John';
-let lastName = 'Doe';
-let fullName = firstName + ' ' + lastName;
-console.log(fullName);
-// getting characters
+// // string concatination
+// let firstName = 'John';
+// let lastName = 'Doe';
+// let fullName = firstName + ' ' + lastName;
+// console.log(fullName);
+// // getting characters
 
-console.log(fullName[0]);
-console.log(fullName[1]);
-console.log(fullName[2]);
-console.log(fullName[3]);
-// string length
-console.log(fullName.length);
-console.log(lastName.length);
-console.log(firstName.length);
-// string methods
-console.log(fullName.toUpperCase());
-let result = fullName.toLowerCase();
+// console.log(fullName[0]);
+// console.log(fullName[1]);
+// console.log(fullName[2]);
+// console.log(fullName[3]);
+// // string length
+// console.log(fullName.length);
+// console.log(lastName.length);
+// console.log(firstName.length);
+// // string methods
+// console.log(fullName.toUpperCase());
+// let result = fullName.toLowerCase();
+// console.log(result);
+// let index = email.indexOf('@');
+// console.log(index);
+
+// // common string methods
+let email = 'amigozabe@gmail.com';
+let result = email.lastIndexOf('a');
 console.log(result);
-let index = email.indexOf('@');
-console.log(index);
+let result1 = email.slice(0, 10);
+console.log(result1);
+let result2 = email.substr(4, 10);
+console.log(result2);
+let result3 = email.replace('a', 'o');
+console.log(result3);
+let result4 = email.replace('g', 'i');
+console.log(result4);
