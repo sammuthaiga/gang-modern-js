69cdfb4bb2b7edf4cd57799229e8b31d0e3d8e97
Samuel <samuel.muthaiga@student.moringaschool.com>
2024-02-10 17:45:12 +0300

    Comparison operators to the check the thruthyness or falseyness of a statement

--- index.js
+++ index.js
@@ -5,11 +5,32 @@ console.log(true, false);
 let email = 'samido@gmail.com';
 let names = ['mario', 'luigi', 'toad'];
 
-let result = email.includes('@');
-let result1 = names.includes('luigi');
+// let result = email.includes('@');
+// let result1 = names.includes('luigi');
 
-console.log(result, result1);
+// console.log(result, result1);
 
+// Comparison operators
+let age = 25;
+
+console.log(age == 25);
+console.log(age == 30);
+console.log(age != 30);
+console.log(age > 20);
+console.log(age < 20);
+console.log(age <= 25);
+console.log(age >= 25);
+
+let name = 'shaun';
+
+console.log(name == 'shaun');
+console.log(name == 'Shaun');
+console.log(name > 'crystal');
+console.log(name > 'Shaun');
+console.log(name > 'Crystal');
+console.log(name > 'shaun');
+console.log(name > 'Shaun');
+console.log(name > 'Crystal');
 
 
 
