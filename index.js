181e9cdc37314bdf667879c576bf226e467f769a
Samuel <samuel.muthaiga@student.moringaschool.com>
2024-02-10 17:18:53 +0300

    Example of basic array methods

--- index.js
+++ index.js
@@ -3,19 +3,53 @@ let ninjas = ['shaun', 'ryu', 'chun-li'];
 console.log(ninjas);
 
 // Arrays can hold different types of data
-ninjas = ['shaun', 'ryu', 'chun-li', 10, 20];
+// ninjas = ['shaun', 'ryu', 'chun-li', 10, 20];
+// console.log(ninjas);
+
+// // Arrays can also hold other arrays
+// let random = ['shaun', 'ryu', 'chun-li', 10, 20, ['one', 'two']];
+// console.log(random);
+
+// // Accessing elements through their index
+// console.log(ninjas[1]);
+
+// // Modifying elements through their index
+// ninjas[1] = 'ken';
+// console.log(ninjas[1]);
+
+// let ages = [20, 25, 30, 35];
+// console.log(ages[2]);
+
+// let random1 = ['shaun', 'ryu', 'chun-li', 10, 20];
+// console.log(random1[4]);
+
+// Array methods and functions
+console.log(ninjas.length);
+
+// Array methods
+// join array method
+let result = ninjas.join(',');
+console.log(result);
+
+// indexOf array method
+let result1 = ninjas.indexOf('chun-li');
+console.log(result1);
+
+// concat array method
+let result2 = ninjas.concat(['ken', 'crystal']);
+console.log(result2);
+
+// push array method
+let result3 = ninjas.push('kene');
+console.log(result3);
 console.log(ninjas);
 
-// Arrays can also hold other arrays
-let random = ['shaun', 'ryu', 'chun-li', 10, 20, ['one', 'two']];
-console.log(random);
+// pop array method
+let result4 = ninjas.pop();
+console.log(result4);
+console.log(ninjas);
 
-// Accessing elements through index
-console.log(ninjas[1]);
-
-// Modifying elements
-ninjas[1] = 'ken';
-console.log(ninjas[1]);
+// Array functions
 
 
 
