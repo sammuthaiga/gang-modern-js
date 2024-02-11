256e8dd68237e69582ca0eb51eac3874917234df
Samuel <samuel.muthaiga@student.moringaschool.com>
2024-02-11 10:12:59 +0300

    password strength evaluation system with nested if-else statements for practice

--- index.js
+++ index.js
@@ -14,21 +14,36 @@
 // while(j < 5);
 
 // if statements
-const age = 25;
-if(age > 20) {
-    console.log('you are over 20 years old');
-}
+// const age = 25;
+// if(age > 20) {
+//     console.log('you are over 20 years old');
+// }
 
-const ninjas = ['shaun', 'ryu', 'chun-li', 'yoshi'];
-if(ninjas.length > 3) {
-    console.log("that's a lot of ninjas");
-}
+// const ninjas = ['shaun', 'ryu', 'chun-li', 'yoshi'];
+// if(ninjas.length > 3) {
+//     console.log("that's a lot of ninjas");
+// }
 
-const password = 'p@ssword';
+const password = 'p@ss';
 
 if(password.length >= 8) {
     console.log('that password is long enough');
+} else {
+    console.log('password is not long enough');
 }
+
+const password1 = "";
+if(password1.length >= 20) {
+    console.log('this is is super strong password');
+    } else if (password1.length >= 12) {
+    console.log('password is very strong');
+    } else if (password1.length >= 8) {
+    console.log('password is long enough');
+    } else if (password1.length >= 6) {
+    console.log('password is too short');
+    } else  if (password1.length <4){
+    console.log("can't find your password! It's too short");
+    }
 
 
 
