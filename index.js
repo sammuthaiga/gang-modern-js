9c0aeba49809fdc0e5d979d276e2c9f3dfcbfc05
Samuel <samuel.muthaiga@student.moringaschool.com>
2024-02-11 09:38:57 +0300

    Practice loops

--- index.js
+++ index.js
@@ -2,16 +2,39 @@
 // Loops and Conditionals
 
 // for Loops
-for (let i = 0; i < 5 ; i++) {
-    console.log('In loop:', i);
-}
-console.log('loop finished');
+// for (let i = 0; i < 5 ; i++) {
+//     console.log('In loop:', i);
+// }
+// console.log('loop finished');
+
+// const names = ['shaun', 'mario', 'luigi'];
+// for (let i = 0; i < names.length; i++) {
+//     // console.log(i);
+//     console.log(names[i]);
+// }
+
+// while loops
+// let i = 0;
+// while(i < 5) {
+//     console.log('in loop:', i);
+//     i++;
+// }
 
 const names = ['shaun', 'mario', 'luigi'];
-for (let i = 0; i < names.length; i++) {
-    // console.log(i);
-    console.log(names[i]);
+let i = 0;
+while(i < names.length) {
+    console.log('in loop:', names[i]);
+    i++;
 }
+
+// do while loops
+let j = 5;
+do {
+    console.log('val of j is:', j);
+    j++;
+}
+while(j < 5);
+
 
 
 
