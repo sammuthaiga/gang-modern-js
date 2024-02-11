16177fd3fad8d1ccc5bff4de1e9c1fa739339903
Samuel <samuel.muthaiga@student.moringaschool.com>
2024-02-11 09:51:31 +0300

    Refined conditional flows in if statements to include age comparison, array length check, and password length validation.

--- index.js
+++ index.js
@@ -1,39 +1,34 @@
-// Control flow
-// Loops and Conditionals
-
-// for Loops
-// for (let i = 0; i < 5 ; i++) {
-//     console.log('In loop:', i);
-// }
-// console.log('loop finished');
-
 // const names = ['shaun', 'mario', 'luigi'];
-// for (let i = 0; i < names.length; i++) {
-//     // console.log(i);
-//     console.log(names[i]);
-// }
-
-// while loops
 // let i = 0;
-// while(i < 5) {
-//     console.log('in loop:', i);
+// while(i < names.length) {
+//     console.log('in loop:', names[i]);
 //     i++;
 // }
 
-const names = ['shaun', 'mario', 'luigi'];
-let i = 0;
-while(i < names.length) {
-    console.log('in loop:', names[i]);
-    i++;
+// // do while loops
+// let j = 5;
+// do {
+//     console.log('val of j is:', j);
+//     j++;
+// }
+// while(j < 5);
+
+// if statements
+const age = 25;
+if(age > 20) {
+    console.log('you are over 20 years old');
 }
 
-// do while loops
-let j = 5;
-do {
-    console.log('val of j is:', j);
-    j++;
+const ninjas = ['shaun', 'ryu', 'chun-li', 'yoshi'];
+if(ninjas.length > 3) {
+    console.log("that's a lot of ninjas");
 }
-while(j < 5);
+
+const password = 'p@ssword';
+
+if(password.length >= 8) {
+    console.log('that password is long enough');
+}
 
 
 
