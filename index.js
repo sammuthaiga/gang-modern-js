f40bb6f26b6618c3e388f49e0ddc8e28d64a1101
Samuel <samuel.muthaiga@student.moringaschool.com>
2024-02-11 11:34:13 +0300

    switch statements for efficient grade evaluation, providing specific console logs based on the given grade.

--- index.js
+++ index.js
@@ -10,3 +10,27 @@ for (let i = 0; i < scores.length; i++) {
         break;
     }
 }
+
+// switch statements.
+const grade = 'D';
+
+// using the switch statements.
+switch (grade) {
+    case 'A':
+        console.log('You got an A!');
+        break;
+    case 'B':
+        console.log('You got a B!');
+        break;
+    case 'C':
+        console.log('You got a C!');
+        break;
+    case 'D':
+        console.log('You got a D!');
+        break;
+    case 'E':
+        console.log('You got an E!');
+        break;
+    default:
+        console.log('Not a valid grade');
+}
