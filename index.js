e0a81d49d505c1933d4d2c6bca43a1b6a397d2ae
Samuel <samuel.muthaiga@student.moringaschool.com>
2024-02-11 11:04:15 +0300

    'Continue' skips processing when encountering zero scores, optimizing output. 'Break' terminates the loop upon reaching the top score, providing clear indication of score evaluation completion.

--- index.js
+++ index.js
@@ -1,65 +1,12 @@
-// const names = ['shaun', 'mario', 'luigi'];
-// let i = 0;
-// while(i < names.length) {
-//     console.log('in loop:', names[i]);
-//     i++;
-// }
-
-// // do while loops
-// let j = 5;
-// do {
-//     console.log('val of j is:', j);
-//     j++;
-// }
-// while(j < 5);
-
-// if statements
-// const age = 25;
-// if(age > 20) {
-//     console.log('you are over 20 years old');
-// }
-
-// const ninjas = ['shaun', 'ryu', 'chun-li', 'yoshi'];
-// if(ninjas.length > 3) {
-//     console.log("that's a lot of ninjas");
-// }
-
-const password = 'p@ss';
-
-if(password.length >= 8) {
-    console.log('that password is long enough');
-} else {
-    console.log('password is not long enough');
-}
-
-const password1 = "";
-if(password1.length >= 20) {
-    console.log('this is is super strong password');
-    } else if (password1.length >= 12) {
-    console.log('password is very strong');
-    } else if (password1.length >= 8) {
-    console.log('password is long enough');
-    } else if (password1.length >= 6) {
-    console.log('password is too short');
-    } else  if (password1.length <4){
-    console.log("can't find your password! It's too short");
+// break and continue
+const scores = [50, 25, 0, 30, 100, 20, 10];
+for (let i = 0; i < scores.length; i++) {
+    if (scores[i] === 0) {
+        continue;
     }
-
-// logical operators - OR || and AND &&
-const password2 = 'p@ssword';
-if(password2.length >= 12 && password2.includes('@')) {
-    console.log('that password is mighty strong');
-} else if(password2.length >= 8 || password2.includes('@') && password2.length > 5) {
-    console.log('that password is strong enough');
-} else {
-    console.log('password is not strong enough');
+    console.log('Your score: ', scores[i]);
+    if (scores[i] === 100) {
+        console.log('Congratulations, you got the top score!');
+        break;
+    }
 }
-
-// logical NOT (!)
-let user = false;
-if(!user) {
-    console.log('you must be logged in to continue');
-} else {
-    console.log('you are logged in');
-}
-
