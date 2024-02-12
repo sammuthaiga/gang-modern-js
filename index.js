99cf37d0a6588aab2c9216cc99cd25b98835ebf5
Samuel <samuel.muthaiga@student.moringaschool.com>
2024-02-13 02:30:37 +0300

    Query the DOM to select elements via the id using document.getElementByClassName

--- index.js
+++ index.js
@@ -6,3 +6,8 @@
 const title = document.getElementById('page-title');
 console.log(title);
 
+// get elements by class name
+const errors = document.getElementsByClassName('error');
+console.log(errors);
+console.log(errors[0]);
+
