8057b32c30bcab5b88f4d9f11e07f8c9b56bae3b
Samuel <samuel.muthaiga@student.moringaschool.com>
2024-02-12 18:15:44 +0300

    Add callback function and forEach examples with logging for an array of people. Also, include a self-practice section to calculate and display payments for plumbers based on the array length.

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
-// functions
+// callback functions
+const myFunc = (callbackFunc) => {
+    let value = 50;
+    callbackFunc(value);
+};
 
-const name = 'Shaun'
-const greet = () => 'hello';
-let result = greet();
-console.log(result);
+myFunc(value => {
+    console.log(value);
+});
 
+// forEach
+let people = ['mario', 'luigi', 'ryu', 'shaun', 'chun-li'];
 
-// methods
-let result1 = name.toUpperCase();
-console.log(result1);
+people.forEach((person, index) => {
+    console.log(index, person);
+});
 
+const logPerson = (person, index) => {
+    console.log(`${index} - hello ${person}`);
+}
 
+people.forEach(logPerson);
+
+// self practice
+const payment = 1000 * people.length;
+
+const payPlumber = (plumber, index,) => {
+    console.log(`${index} - hello ${plumber} here is your payment of ${payment}`);
+}
+
+people.forEach(payPlumber);
