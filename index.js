62dce7d0f769c7d3d10fcdeaaf1070fab4bf6d63
Samuel <samuel.muthaiga@student.moringaschool.com>
2024-02-12 18:26:10 +0300

    Implement dynamic HTML list creation for an array of people using forEach and a callback function

--- index.js
+++ index.js
@@ -1,31 +1,14 @@
-// callback functions
-const myFunc = (callbackFunc) => {
-    let value = 50;
-    callbackFunc(value);
-};
+// get a reference to the ul
+const ul = document.querySelector('.people');
 
-myFunc(value => {
-    console.log(value);
+const people = ['mario', 'luigi', 'yoshi'];
+
+let html = ``;
+
+people.forEach(person => {
+  // create html template
+  html += `<li style="color: purple">${person}</li>`;
 });
 
-// forEach
-let people = ['mario', 'luigi', 'ryu', 'shaun', 'chun-li'];
-
-people.forEach((person, index) => {
-    console.log(index, person);
-});
-
-const logPerson = (person, index) => {
-    console.log(`${index} - hello ${person}`);
-}
-
-people.forEach(logPerson);
-
-// self practice
-const payment = 1000 * people.length;
-
-const payPlumber = (plumber, index,) => {
-    console.log(`${index} - hello ${plumber} here is your payment of ${payment}`);
-}
-
-people.forEach(payPlumber);
+console.log(html);
+ul.innerHTML = html;
