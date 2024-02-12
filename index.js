749000172bcbe93349d2dcf11e2509d0d6f00d40
Samuel <samuel.muthaiga@student.moringaschool.com>
2024-02-12 23:31:23 +0300

    shorthand syntax for object methods, emphasize the significance of the 'this' keyword, and demonstrate accessing key/value pairs within object methods.

--- index.js
+++ index.js
@@ -1,29 +1,52 @@
-// objects literals notation is a way to create objects in JavaScript.
-let user = {
-    name: 'John',
+// objects 
+
+// Objects methods
+let user2 = {
+    name: 'Sam',
     age: 30,
-    email: 'john@example.com',
+    email: 'sam@example.com',
     location: 'USA',
-    blogs: ['why mac & cheese rules', '10 things to make with marmite']
+    blogs: ['why mac & cheese rules', '10 things to make with marmite'],
+    login: function(){
+        console.log('the user logged in');
+    },
+    logout: function(){
+        console.log('the user logged out');
+    }
 };
 
-console.log(user);
+user2.login();
+user2.logout();
 
-// Accessing the properties of an object using dot notation.
-console.log(user.name);
+// The 'this' keyword
+let user3 =
+{
+    name: 'Shaun',
+    age: 30,
+    email: 'shaun@gmail.com',
+    location: 'USA',
+    blogs: ['why mac & cheese rules', '10 things to make with marmite'],
 
-// Changing the value of a property of an object using dot notation.
-user.age = 35;
-console.log(user.age);
+    // use shorthand regular function syntax to define a method in an object instead of using the function keyword.
+    login(){
+        console.log('the user logged in');
+    },
+    logout(){
+        console.log('the user logged out');
+    },
 
-// Accessing the properties of an object using square brackets.
-console.log(user['name']);
+    // this is a method that logs the blogs of the user
+    // this keyword is used to refer to the object that the method is called on.
+    // this keyword does not work with arrow functions.
 
-// Changing the value of a property of an object using square brackets.
-user['name'] = 'Chun-li';
-console.log(user['name']);
+    logBlogs(){
+        console.log('this user has written the following blogs:');
+        this.blogs.forEach(blog => {
+            console.log(blog);
+        });
+    }
+};
 
+user3.logBlogs();
 
-console.log(typeof user);
-
-
+// Math object
