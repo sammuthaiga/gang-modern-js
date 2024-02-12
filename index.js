688f99f4ed7aac721f55c5b5e881dd1bbf1242e1
Samuel <samuel.muthaiga@student.moringaschool.com>
2024-02-13 01:20:19 +0300

    Demonstrate the difference between primitive and reference values in JavaScript. Show that primitive values create independent copies, while reference values share the same object in memory. Emphasize the need for specific methods to create true copies of objects. Provide concise code examples for both scenarios.

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
-// Math object
-console.log(Math);
-console.log(Math.PI);
-console.log(Math.E);
+// primitive values
 
-const area = 7.7;
-console.log(Math.round(area));
-console.log(Math.floor(area));
-console.log(Math.ceil(area));
-console.log(Math.trunc(area));
+// primitive values in JavaScript, such as numbers, strings, booleans, null, and undefined, are stored in the stack when they are part of a variable. However, when a primitive value is copied to another variable, the value itself is copied, not the reference.
 
-// random numbers
-const random = Math.random();
-console.log(random);
-console.log(Math.round(random * 100));
+// 
+let scoreOne = 50;
+let scoreTwo = scoreOne;
+console.log(`scoreOne: ${scoreOne}`, `scoreTwo: ${scoreTwo}`);
 
-// Reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
+// when you copy a primitive value, you create a new independent copy of that value in a separate stack space.
+scoreOne = 100;
+console.log(`scoreOne: ${scoreOne}`, `scoreTwo: ${scoreTwo}`);
+
+// reference values
+// reference values in JavaScript, such as arrays and objects, are stored in the heap when they are part of a variable. When a reference value is copied to another variable, the reference is copied, not the value itself.
+const userOne = { name: 'ryu', age: 30 };
+const userTwo = userOne;
+console.log(userOne, userTwo);
+
+// when you copy a reference value, you create a new reference to the same object in the heap.
+userOne.age = 40;
+console.log(userOne, userTwo);
+
 
