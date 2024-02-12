359d5ce9a83715407b5fd76b544d8993ab10cf55
Samuel <samuel.muthaiga@student.moringaschool.com>
2024-02-13 02:40:49 +0300

    Changing the content of an element using INNERHTML

--- index.js
+++ index.js
@@ -1,20 +1,7 @@
 // Document Object Model (DOM) is a programming interface for HTML and XML documents. It represents the page so that programs can change the document structure, style, and content. The DOM represents the document as nodes and objects. That way, programming languages can connect to the page.
 
-
-// Querying the DOM
-// get an element by ID 
-const title = document.getElementById('page-title');
-console.log(title);
-
-// get elements by class name
-const errors = document.getElementsByClassName('error');
-console.log(errors);
-console.log(errors[0]);
-
-// get elements by their tag name
-const paras = document.getElementsByTagName('p');
-console.log(paras);
-console.log(paras[1]);
-
+const para = document.querySelector('p');
+console.log(para.innerText);
+para.innerText = 'Ninjas are awesome!';
 
 
