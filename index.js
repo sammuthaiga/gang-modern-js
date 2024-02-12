85cf82a14d2bf2320555018019955732ae35d9a5
Samuel <samuel.muthaiga@student.moringaschool.com>
2024-02-12 17:38:35 +0300

    working with methods. Methods are used on other data types while functions are used on their own

--- index.js
+++ index.js
@@ -1,28 +1,13 @@
-// Arrow functions
-const calArea = (radius) => {
-    return 3.14 * radius**2;
-};
+// functions
 
-const area = calArea(5);
-console.log('Area is:', area);
+const name = 'Shaun'
+const greet = () => 'hello';
+let result = greet();
+console.log(result);
 
-// Arrow functions with one parameter
-const greet = name => 'Hello ' + name;
-console.log(greet('Mario'));
 
-// Arrow functions with multiple parameters
-const bill = (products, tax) => {
-    let total = 0;
-    for (let i = 0; i < products.length; i++) {
-        total += products[i] + products[i] * tax;
-    }
-    return total;
-};
-console.log(bill([10, 15, 30], 0.2));
-
-// Arrow functions with no parameters
-const name = 'Shaun';
-const greet1 = () => 'Hello ' + name;
-console.log(greet1());
+// methods
+let result1 = name.toUpperCase();
+console.log(result1);
 
 
