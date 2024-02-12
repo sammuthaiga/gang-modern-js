c8454d3979db409a1d9563a0a6ceaae253e0407e
Samuel <samuel.muthaiga@student.moringaschool.com>
2024-02-12 22:56:33 +0300

    Create and manipulate an object using object literals notation in JavaScript. Access object properties using both dot notation and square brackets. Demonstrate changing the values of object properties. Finally, display the data type of the object.

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
-// get a reference to the ul
-const ul = document.querySelector('.people');
+// objects literals notation is a way to create objects in JavaScript.
+let user = {
+    name: 'John',
+    age: 30,
+    email: 'john@example.com',
+    location: 'USA',
+    blogs: ['why mac & cheese rules', '10 things to make with marmite']
+};
 
-const people = ['mario', 'luigi', 'yoshi'];
+console.log(user);
 
-let html = ``;
+// Accessing the properties of an object using dot notation.
+console.log(user.name);
 
-people.forEach(person => {
-  // create html template
-  html += `<li style="color: purple">${person}</li>`;
-});
+// Changing the value of a property of an object using dot notation.
+user.age = 35;
+console.log(user.age);
 
-console.log(html);
-ul.innerHTML = html;
+// Accessing the properties of an object using square brackets.
+console.log(user['name']);
+
+// Changing the value of a property of an object using square brackets.
+user['name'] = 'Chun-li';
+console.log(user['name']);
+
+
+console.log(typeof user);
+
+
