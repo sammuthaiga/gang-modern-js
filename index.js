e1194872dbe8c6e34aaa8179dd901fd5783f4e57
Samuel <samuel.muthaiga@student.moringaschool.com>
2024-02-13 16:30:53 +0300

    Update HTML element attributes: set and get 'href', modify 'class', and add an inline style.

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ const link = document.querySelector('a');
 console.log(link.getAttribute('href'));
 link.setAttribute('href', 'https://www.thenetninja.co.uk');
 link.innerText = 'The Net Ninja Website';
+
+const message = document.querySelector('p');
+console.log(message.getAttribute('class')); // get the class attribute
+message.setAttribute('class', 'success'); // set the class attribute to success
+message.setAttribute('style', 'color: green;'); // inline styles
