b6c542fada9c980b036b61f16b30733d5848c299
Samuel <samuel.muthaiga@student.moringaschool.com>
2024-02-13 17:08:24 +0300

    Manage HTML element classes with classList. Add error, remove it, and add success. Display the updated classList.

--- index.js
+++
@@ -1,8 +0,0 @@
-// style property
-const title = document.querySelector('h1'); // select the first h1 element 
-console.log(title.style); // style property
-console.log(title.style.color); // style color property 
-title.style.color = 'red'; // change the color to red
-title.style.fontSize = '60px'; // change the font size to 60px
-title.style.margin = '20px'; // change the margin to 20px
-title.style.margin = ''; // remove the margin property
