e17d14fca6c32d12a0453b115eb1862941cc2575
Samuel <samuel.muthaiga@student.moringaschool.com>
2024-02-13 21:19:55 +0300

    Toggle the 'test' class on the HTML element with the class 'title' using classList.toggle.

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-const paras = document.querySelectorAll('p');
-paras.forEach(para => {
-  if (para.textContent.includes('success')) {
-    para.classList.add('success');
-  } else if (para.textContent.includes('error')) {
-    para.classList.add('error');
-  } else if (para.textContent.includes('warning')) {
-    para.classList.add('warning');
-  }
-});
+
+
+// toggle a class
+const title = document.querySelector('.title');
+title.classList.toggle('test');
+
